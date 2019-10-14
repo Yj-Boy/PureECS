7422ed2b2f8c1bfb54d43e2c513178fc2b9e26fd
Yj-Boy <1529008728@qq.com>
2019-10-14 13:04:22 +0800

    增加RemoveDeadSystem

---
+++ Assets/Scritps/RemovDeadSystem.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+
+public class RemovDeadSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Entity entity, ref Times times) =>
+        {
+            if (times.timeByComponet > 2)
+            {
+                PostUpdateCommands.DestroyEntity(entity);
+            }
+        });
+    }
+}
